9c1722a9411f5a9e3516a49488930d328b54d04c
Francis <francisjd67@gmail.com>
2024-01-08 14:37:19 -0500

    my first commit

---
+++ FYmycode.py
@@ -0,0 +1,9 @@
+def main() :
+    print("What is your name?" ,end="")
+    name = input()
+    answer = input("We want to know if you like coding!" + name +"?")
+    answer = input("Do you like it "+ name + "?")
+    print("Wow of course you said " + answer + "?")
+    print("I think you like coding!")
+if __name__ == '__main__':
+        main()
---
+++ PYmycode.py
@@ -0,0 +1,8 @@
+def main() :
+    name = input("What is your name?")
+    print("We want to know if you like programming!")
+    answer = input("Do you like programming"+ name + "?")
+    print('Wow of course you said "' + answer + '"?')
+    print("I think you're lame!")
+if __name__ == '__main__':
+        main()
